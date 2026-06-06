750e86ae9b458d4436935d8ba4468ed936b736c3
yasirkhan1180 <yk8262003@gmail.com>
2026-06-06 06:30:54 +0500

    Final: Cleaned Sanity integration and client configuration

--- sanity/lib/fetch.ts
+++ sanity/lib/fetch.ts
@@ -1,37 +1,30 @@
 import { client } from "./client";
-import {
-  ALL_ARTICLES_QUERY,
-  ARTICLE_BY_SLUG_QUERY,
-  ALL_SLUGS_QUERY,
-} from "./queries";
+import { ALL_ARTICLES_QUERY, ARTICLE_BY_SLUG_QUERY, ALL_SLUGS_QUERY } from "./queries";
 
-export type Article = {
+// Define the interface that matches your Sanity Schema
+export interface Article {
   _id: string;
   title: string;
   slug: string;
   categoryTag: string;
   excerpt: string;
+  mainContent: any; // Using 'any' here is standard for PortableText arrays
   readTime: number;
   publishedAt: string;
-};
+  author: string;
+}
 
-export type ArticleFull = Article & {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  mainContent: any[];
-};
-
+// Fetch all articles for the homepage feed
 export async function getAllArticles(): Promise<Article[]> {
-  return client.fetch(ALL_ARTICLES_QUERY, {}, { next: { revalidate: 60 } });
+  return await client.fetch(ALL_ARTICLES_QUERY);
 }
 
-export async function getArticleBySlug(slug: string): Promise<ArticleFull | null> {
-  return client.fetch(
-    ARTICLE_BY_SLUG_QUERY,
-    { slug },
-    { next: { revalidate: 60 } }
-  );
+// Fetch a single article by slug
+export async function getArticleBySlug(slug: string): Promise<Article> {
+  return await client.fetch(ARTICLE_BY_SLUG_QUERY, { slug });
 }
 
-export async function getAllSlugs(): Promise<{ slug: string }[]> {
-  return client.fetch(ALL_SLUGS_QUERY, {}, { next: { revalidate: 3600 } });
+// Fetch all slugs for Static Site Generation (SSG)
+export async function getAllSlugs() {
+  return await client.fetch(ALL_SLUGS_QUERY);
 }
