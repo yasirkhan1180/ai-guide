4e3c7f9c619923e3a8c7f60691c2fe16d8cc27e9
yasirkhan1180 <yk8262003@gmail.com>
2026-06-06 06:15:03 +0500

    Final: Fix GROQ queries and remove duplication

--- sanity/lib/utils.ts
+++ sanity/lib/utils.ts
@@ -1,7 +1,34 @@
-export function formatDate(date: string) {
-  return new Date(date).toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
-}
+import { groq } from "next-sanity";
+
+export const ALL_ARTICLES_QUERY = groq`
+  *[_type == "article" && defined(publishedAt)] | order(publishedAt desc) {
+    _id,
+    title,
+    "slug": slug.current,
+    categoryTag,
+    excerpt,
+    readTime,
+    publishedAt,
+    author
+  }
+`;
+
+export const ARTICLE_BY_SLUG_QUERY = groq`
+  *[_type == "article" && slug.current == $slug && defined(publishedAt)][0] {
+    _id,
+    title,
+    "slug": slug.current,
+    categoryTag,
+    excerpt,
+    mainContent,
+    readTime,
+    publishedAt,
+    author
+  }
+`;
+
+export const ALL_SLUGS_QUERY = groq`
+  *[_type == "article" && defined(publishedAt)] {
+    "slug": slug.current
+  }
+`;
